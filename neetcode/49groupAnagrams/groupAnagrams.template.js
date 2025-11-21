7b858bf30a98223b7d2c28b52d9c682db7c0f30d
Victor Patru <patruvictor.mihai@gmail.com>
2025-11-21 17:05:05 +0200

    fix: add details to solve the group anagram

--- neetcode/49groupAnagrams/groupAnagrams.template.js
+++ neetcode/49groupAnagrams/groupAnagrams.template.js
@@ -1,6 +1,36 @@
 /**
- * Solution 1: Using character frequency counting (hash map approach)
+ * Group Anagrams
+ *
+ * Given an array of strings strs, group all anagrams together into sublists.
+ * You may return the output in any order.
+ *
+ * An anagram is a string that contains the exact same characters as another
+ * string, but the order of the characters can be different.
+ *
+ * @example
+ * // Example 1:
+ * // Input: strs = ["act","pots","tops","cat","stop","hat"]
+ * // Output: [["hat"],["act", "cat"],["stop", "pots", "tops"]]
+ *
+ * @example
+ * // Example 2:
+ * // Input: strs = ["x"]
+ * // Output: [["x"]]
+ *
+ * @example
+ * // Example 3:
+ * // Input: strs = [""]
+ * // Output: [[""]]
+ *
+ * @constraints
+ * - 1 <= strs.length <= 1000
+ * - 0 <= strs[i].length <= 100
+ * - strs[i] is made up of lowercase English letters
+ * 
+ * Solution Using character frequency counting (hash map approach)
+ * Hint: Create a key from the character count array (by joining it)
  */
+
 class Solution {
     /**
      * @param {string[]} strs
@@ -21,4 +51,3 @@ if (typeof module !== 'undefined' && module.exports) {
     module.exports = { Solution };
 }
 
-// Hint: Create a key from the character count array (by joining it)
