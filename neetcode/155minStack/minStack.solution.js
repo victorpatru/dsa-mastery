f3ec1f8e4d50b2c6489c5ea4602aa0e9e85465ea
Victor Patru <patruvictor.mihai@gmail.com>
2025-11-29 15:11:09 +0200

    fix: make the solution cleaner to write

--- neetcode/155minStack/minStack.solution.js
+++ neetcode/155minStack/minStack.solution.js
@@ -11,13 +11,10 @@ class MinStack {
      */
     push(val) {
         this.stack.push(val);
-        val = Math.min(
-            val,
-            this.minStack.length === 0
-                ? val
-                : this.minStack[this.minStack.length - 1],
-        );
-        this.minStack.push(val);
+        const min = this.minStack.length === 0
+            ? val
+            : Math.min(val, this.minStack[this.minStack.length - 1]);
+        this.minStack.push(min);
     }
 
     /**
