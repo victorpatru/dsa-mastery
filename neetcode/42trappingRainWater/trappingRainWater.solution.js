b0ada89dc7203ae7014bb4908a77903bd0d2325e
Victor Patru <patruvictor.mihai@gmail.com>
2025-11-29 15:35:55 +0200

    fix: make init more concise

--- neetcode/42trappingRainWater/trappingRainWater.solution.js
+++ neetcode/42trappingRainWater/trappingRainWater.solution.js
@@ -8,10 +8,8 @@ class Solution {
         if (!height || height.length === 0) {
             return 0;
         }
-        let l = 0;
-        let r = height.length - 1;
-        let leftMax = height[l];
-        let rightMax = height[r];
+        let l = 0, r = height.length - 1;
+        let leftMax = height[l], rightMax = height[r];
         let res = 0;
         while (l < r) {
             if (leftMax < rightMax) {
