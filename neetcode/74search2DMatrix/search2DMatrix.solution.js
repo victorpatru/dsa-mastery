c4a9cafcae0ad16ae2dce76c50c3355de6d8ed0d
Victor Patru <patruvictor.mihai@gmail.com>
2025-12-28 23:41:32 +0200

    chore: integer overflow friendly way of finding the middle

--- neetcode/74search2DMatrix/search2DMatrix.solution.js
+++ neetcode/74search2DMatrix/search2DMatrix.solution.js
@@ -11,7 +11,7 @@ class Solution {
         let top = 0;
         let bot = ROWS - 1;
         while (top <= bot) {
-            const midRow = Math.floor((top + bot) / 2);
+            const midRow = bot + Math.floor((top - bot) / 2);
             if (target > matrix[midRow][COLS - 1]) {
                 top = midRow + 1;
             } else if (target < matrix[midRow][0]) {
@@ -24,11 +24,11 @@ class Solution {
         if (!(top <= bot)) {
             return false;
         }
-        const row = Math.floor((top + bot) / 2);
+        const row = bot + Math.floor((top - bot) / 2);
         let l = 0;
         let r = COLS - 1;
         while (l <= r) {
-            const m = Math.floor((l + r) / 2);
+            const m = l + Math.floor((r - l) / 2);
             if (target > matrix[row][m]) {
                 l = m + 1;
             } else if (target < matrix[row][m]) {
