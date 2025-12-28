453da253c858e50e88f6a156411e93a9f2c2acb0
Victor Patru <patruvictor.mihai@gmail.com>
2025-12-28 23:37:55 +0200

    chore: improve naming

--- neetcode/74search2DMatrix/search2DMatrix.solution.js
+++ neetcode/74search2DMatrix/search2DMatrix.solution.js
@@ -11,11 +11,11 @@ class Solution {
         let top = 0;
         let bot = ROWS - 1;
         while (top <= bot) {
-            const row = Math.floor((top + bot) / 2);
-            if (target > matrix[row][COLS - 1]) {
-                top = row + 1;
-            } else if (target < matrix[row][0]) {
-                bot = row - 1;
+            const midRow = Math.floor((top + bot) / 2);
+            if (target > matrix[midRow][COLS - 1]) {
+                top = midRow + 1;
+            } else if (target < matrix[midRow][0]) {
+                bot = midRow - 1;
             } else {
                 break;
             }
